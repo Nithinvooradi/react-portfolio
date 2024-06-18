82319e469ef510c07ce7651a58587184b5e6af1f
Nithinvooradi <vooradinithin@gmail.com>
2024-06-18 13:43:42 +0530

    small refactor

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -32,7 +32,6 @@ const SocialLinks = () => {
         </>
       ),
       href: "mailto:vooradinithin@gmail.com",
-      
     },
     {
       id: 4,
@@ -43,28 +42,33 @@ const SocialLinks = () => {
       ),
       href: "/VooradiNithin.pdf",
       style: "rounded-br-md",
-      download:true,
+      download: true,
     },
   ];
   return (
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed ">
       <ul>
-        {Links.map(({id,child,href,style,download}) => (
-            // eslint-disable-next-line
-             <li  key= {id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 " + " " + style}>
-             <a
-               href={href}
-               className="flex justify-between items-center w-full text-white "
-               download={download}
-               target = '_blank'
-               rel="noreferrer"
-             >
-               {child}
-             </a>
-           </li>
-
+        {Links.map(({ id, child, href, style, download }) => (
+          // eslint-disable-next-line
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 " +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white "
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
         ))}
-
       </ul>
     </div>
   );
